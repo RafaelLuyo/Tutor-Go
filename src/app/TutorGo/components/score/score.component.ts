6d7cdc70468ab6788306c44564998be7c7f8f643
Rafael Luyo <u201919295@upc.edu.pe>
2024-05-01 18:56:47 -0500

    feat(score): added score component

---
+++ src/app/TutorGo/components/score/score.component.ts
@@ -0,0 +1,77 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+@Component({
+  selector: 'app-score',
+  templateUrl: './score.component.html',
+  styleUrl: './score.component.css'
+})
+export class ScoreComponent {
+  scores: any[] = [];
+  studentId: number | null = null;
+
+  tabs = [
+    { title: 'All' },
+    { title: 'Graded' },
+    { title: 'Submitted' }
+  ];
+
+  constructor(private route: ActivatedRoute, private scoreService: ScoreService) { }
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.studentId = +params['id'];
+      this.fetchScoresAndStudent();
+    });
+  }
+
+  fetchScoresAndStudent() {
+    this.scoreService.getAll().subscribe(scores => {
+      this.scores = scores;
+    });
+  }
+
+  getScores(tabTitle: string) {
+    if (tabTitle === 'Graded') {
+      return this.scores.filter(score => score.status === 'GRADED');
+    } else if (tabTitle === 'Submitted') {
+      const gradedScores = this.scores.filter(score => score.status === 'GRADED');
+      const submittedScores = this.scores.filter(score => score.status === 'SUBMITTED');
+      return submittedScores.concat(gradedScores);
+    } else {
+      return this.scores;
+    }
+  }
+
+  calculateUserTotal(tabTitle: string) {
+    const userScores = this.getScores(tabTitle);
+    const userTotal = userScores.reduce((total, score) => {
+      if (score.scoreValue) {
+        const scoreParts = score.scoreValue.split('/');
+        if (scoreParts.length === 2) {
+          return total + Number(scoreParts[0]);
+        }
+      }
+      return total;
+    }, 0);
+    return userTotal + '/' + this.calculateAvailableTotal(tabTitle);
+  }
+
+  calculateAvailableTotal(tabTitle: string) {
+    const availableScores = this.getScores(tabTitle);
+    const totalAvailable = availableScores.reduce((total, score) => {
+      if (score.scoreValue) {
+        const scoreParts = score.scoreValue.split('/');
+        if (scoreParts.length === 2) {
+          return total + Number(scoreParts[1]);
+        }
+      }
+      return total;
+    }, 0);
+    return totalAvailable;
+  }
+
+  formatDate(date: string) {
+
+    return date;
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { ScoreComponent } from './tutorgo/components/score/score.component';
+import {MatTab, MatTabGroup} from "@angular/material/tabs";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScoreComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatTab,
+    MatTabGroup
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
