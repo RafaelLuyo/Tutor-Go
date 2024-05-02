5b6e31837d4e7c615d423f56807883e5e9561e86
Rafael Luyo <u201919295@upc.edu.pe>
2024-05-01 19:44:53 -0500

    feat(header): header and sidebar component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ScoreComponent} from "./TutorGo/components/score/score.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'score', component: ScoreComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
