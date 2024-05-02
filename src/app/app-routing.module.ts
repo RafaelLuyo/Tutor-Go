c441e96f9c8d34c9bb43f15efc84d2e63e42414e
Rafael Luyo <u201919295@upc.edu.pe>
2024-05-01 20:24:01 -0500

    feat/ca(calendar):: calendar component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {ScoreComponent} from "./TutorGo/components/score/score.component";
 import {ScheduleComponent} from "./TutorGo/components/schedule/schedule.component";
+import {CalendarComponent} from "./TutorGo/components/calendar/calendar.component";
 
 const routes: Routes = [
   { path: 'score', component: ScoreComponent},
   { path: 'schedule', component: ScheduleComponent},
+  { path: 'calendar', component: CalendarComponent},
 ];
 
 @NgModule({
